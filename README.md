f22be62deb691d45b3c30832916de9cbfb536f3c
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2023-12-16 16:25:22 +0530

    pixi setup and wepack configuration with basic example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learning_pixi
+
 Diving Deep into core of pixi
+
+## Pixi is 2D rendering engine
---
+++ index.js
@@ -0,0 +1,18 @@
+import * as PIXI from "pixi.js";
+
+// Your PixiJS code goes here
+
+// Create a PixiJS application
+const app = new PIXI.Application({ width: 800, height: 600 });
+
+// Add the app view to the DOM
+document.body.appendChild(app.view);
+
+// Create a red square
+const square = new PIXI.Graphics();
+square.beginFill(0xff0000);
+square.drawRect(0, 0, 100, 100);
+square.endFill();
+
+// Add the square to the stage
+app.stage.addChild(square);

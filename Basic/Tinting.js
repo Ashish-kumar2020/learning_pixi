e9913600c1ab15d4f7187261550ae98c0cc4ca53
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2023-12-18 23:35:56 +0530

    tinting using pixi example

--- Basic/Tinting.js
+++ Basic/Tinting.js
@@ -1,19 +1,52 @@
 import * as PIXI from "pixi.js";
 
 const app = new PIXI.Application({
-  backgroundAlpha: 0,
   resizeTo: window,
 });
 
 document.body.appendChild(app.view);
 
-const bunny = PIXI.Sprite.from("https://pixijs.com/assets/bunny.png");
-bunny.anchor.set(0.5);
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
-bunny.tint = 0x0000ff;
-app.stage.addChild(bunny);
+let aliens = [];
+const totalDudes = 20;
+for (let i = 0; i < totalDudes; i++) {
+  const dudes = PIXI.Sprite.from("https://pixijs.com/assets/eggHead.png");
+  dudes.anchor.set(0.5);
+  dudes.scale.set(0.8 + Math.random() * 0.3);
+  dudes.x = Math.random() * app.screen.width;
+  dudes.y = Math.random() * app.screen.height;
+  dudes.tint = Math.random() * 0xffffff;
+  dudes.direction = Math.random() * Math.PI * 2;
+  dudes.turningSpeed = Math.random() - 0.8;
+  dudes.speed = 2 + Math.random() * 2;
+  aliens.push(dudes);
+  app.stage.addChild(dudes);
+}
+const dudeBoundsPadding = 100;
+const dudeBounds = new PIXI.Rectangle(
+  -dudeBoundsPadding,
+  -dudeBoundsPadding,
+  app.screen.width + dudeBoundsPadding * 2,
+  app.screen.height + dudeBoundsPadding * 2
+);
 
-app.ticker.add((delta) => {
-  bunny.rotation += 0.2 * delta;
+app.ticker.add(() => {
+  for (let i = 0; i < aliens.length; i++) {
+    const dude = aliens[i];
+    dude.direction += dude.turningSpeed * 0.01;
+    dude.x += Math.sin(dude.direction) * dude.speed;
+    dude.y += Math.cos(dude.direction) * dude.speed;
+    dude.rotation = -dude.direction - Math.PI / 2;
+
+    if (dude.x < dudeBounds.x) {
+      dude.x += dudeBounds.width;
+    } else if (dude.x > dudeBounds.x + dudeBounds.width) {
+      dude.x -= dudeBounds.width;
+    }
+
+    if (dude.y < dudeBounds.y) {
+      dude.y += dudeBounds.height;
+    } else if (dude.y > dudeBounds.y + dudeBounds.height) {
+      dude.y -= dudeBounds.height;
+    }
+  }
 });

cf3e5b97c7d1ac4c87718bd1e1743583382f4680
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2023-12-18 23:17:14 +0530

    learn about tiniting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./Basic/TransparentBackground.js",
+  entry: "./Basic/Tinting.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
